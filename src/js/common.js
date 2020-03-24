5452bd1e8844d0923276b25b06eb252384eba668
IzvekovWeb <izvekov.web@yandex.ru>
2020-03-24 21:31:56 +0300

    Адаптирован под мобильные

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,16 @@
 // let add = (a,b) => a+b
 // console.log(add(8,3))
+
+document.getElementById('show-menu').addEventListener("click", function(){
+  let mobMenu = document.getElementById('mob-menu');
+  mobMenu.style.display = (mobMenu.style.display == 'none') ? 'flex' : 'none'
+})
+var closeMenu = function(){
+  let mobMenu = document.getElementById('mob-menu');
+  mobMenu.style.display = 'none';
+}
+
+var elements = document.getElementsByClassName("close-menu");
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', closeMenu, false);
+}
